418ab3364f9b3f36f4b4514b38270144640d7f10
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-12 08:10:01 +0530

    -r

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import requests
 
 
 
-x = requests.get(url="http://api.open-notify.org/iss-now.json")
-json_data = x.json()
+# x = requests.get(url="http://api.open-notify.org/iss-now.json")
+# json_data = x.json()
+# print(json_data)
+
+SunRiseandSunSet = requests.get(url="https://api.sunrisesunset.io/json?lat=-37.7636&lng=-171.7346")
+
+json_data = SunRiseandSunSet.json()
+print(json_data)
